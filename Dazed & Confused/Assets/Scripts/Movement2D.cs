e9e0702c9a921549c3c231b9b3288a597d6a8a43
Bryce Tham <brycetham@gmail.com>
2016-10-21 18:06:03 -0700

    Added Movement

---
+++ Dazed & Confused/Assets/Scripts/Movement2D.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement2D : MonoBehaviour {
+	
+	private Rigidbody2D rigidbody;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rigidbody.velocity = movement * speed;
+	}
+}
