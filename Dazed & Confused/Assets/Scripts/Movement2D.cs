e5c0e3630786a636b68245d7001d1f66c6bf0e9f
Bryce Tham <brycetham@gmail.com>
2016-10-21 18:35:52 -0700

    Added Shoot

--- Dazed & Confused/Assets/Scripts/Movement2D.cs
+++ Dazed & Confused/Assets/Scripts/Movement2D.cs
@@ -1,23 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary 
+{
+	public float xMin, xMax, yMin, yMax;
+}
+
 public class Movement2D : MonoBehaviour {
 	
-	private Rigidbody2D rigidbody;
+	private Rigidbody2D rigidbody2d;
 
 	public float speed;
+	public Boundary boundary;
 
-	// Use this for initialization
 	void Start () {
-		rigidbody = GetComponent<Rigidbody2D> ();
+		rigidbody2d = GetComponent<Rigidbody2D> ();
 	}
 
-	// Update is called once per frame
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rigidbody.velocity = movement * speed;
+		rigidbody2d.velocity = movement * speed;
+
+		rigidbody2d.position = new Vector2
+		(
+			Mathf.Clamp (rigidbody2d.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp (rigidbody2d.position.y, boundary.yMin, boundary.yMax)
+		);
 	}
 }
---
+++ Dazed & Confused/Assets/Scripts/Shoot.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	private Rigidbody2D rigidbody2d;
+
+	public float speed;
+
+	void Start () {
+		rigidbody2d = GetComponent<Rigidbody2D> ();
+		rigidbody2d.velocity = transform.up * speed;	
+	}
+}
