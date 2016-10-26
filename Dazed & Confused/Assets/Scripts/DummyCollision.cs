d39d2e244279cb1194a2bc60a07f6d6c432d9d30
Luca-Gazzola <lgazzola@uci.edu>
2016-10-25 19:05:46 -0700

    Added Joe for real, updated .gitignore

---
+++ Dazed & Confused/Assets/Scripts/DummyCollision.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyCollision : MonoBehaviour
+{
+    public bool stun;
+    public float stunTime;
+
+    private float stunEnd;
+
+    void Start() {
+        stun = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "Bullet(Clone)")
+        {
+            Destroy(coll.gameObject);
+            if (!stun)
+            {
+                stun = true;
+                stunEnd = Time.time + stunTime;
+            };
+        }
+    }
+
+    void Update() {
+        if (Time.time > stunEnd)
+        {
+            stun = false;
+        }
+    }
+}
