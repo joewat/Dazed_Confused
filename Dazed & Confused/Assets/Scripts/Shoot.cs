68c75089659dba6a7274936909b6478a6e310a56
Luca-Gazzola <lgazzola@uci.edu>
2016-10-25 18:51:02 -0700

    Added Stun state and collision for bullets
    Added Dummy Joe

--- Dazed & Confused/Assets/Scripts/Shoot.cs
+++ Dazed & Confused/Assets/Scripts/Shoot.cs
@@ -7,8 +7,16 @@ public class Shoot : MonoBehaviour {
 
 	public float speed;
 
-	void Start () {
+    public Boundary boundary;
+
+    void Start () {
 		rigidbody2d = GetComponent<Rigidbody2D> ();
 		rigidbody2d.velocity = transform.up * speed;	
 	}
+
+    void Update() {
+        if (rigidbody2d.position.x < boundary.xMin || rigidbody2d.position.x > boundary.xMax || rigidbody2d.position.y < boundary.yMin || rigidbody2d.position.y > boundary.yMax) {
+            Destroy(this.gameObject);
+        }
+    }
 }
