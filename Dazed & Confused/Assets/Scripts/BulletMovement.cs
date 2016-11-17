aa620948de87281858a56ec419f7290505ee4d84
Bryce Tham <brycetham@gmail.com>
2016-11-16 19:49:23 -0800

    Completed 2-Player Support

--- Dazed & Confused/Assets/Scripts/BulletMovement.cs
+++ Dazed & Confused/Assets/Scripts/BulletMovement.cs
@@ -7,24 +7,13 @@ public class BulletMovement : MonoBehaviour
 	private Rigidbody2D rigidbody2d;
 
 	public float speed;
-    public int playerID;
-
-    public Boundary boundary;
+	public int playerID;
 
 	void Start ()
 	{
 		// Initialize bullet movement speed.
 		rigidbody2d = GetComponent<Rigidbody2D> ();
 		rigidbody2d.velocity = transform.up * speed;	
-	}
-
-	void Update ()
-	{
-		// Destroy bullet if OOB.
-		if (rigidbody2d.position.x < boundary.xMin || rigidbody2d.position.x > boundary.xMax ||
-		          rigidbody2d.position.y < boundary.yMin || rigidbody2d.position.y > boundary.yMax) {
-			Destroy (this.gameObject);
-		}
 	}
 
 }
--- Dazed & Confused/Assets/Scripts/Dash.cs
+++ Dazed & Confused/Assets/Scripts/Dash.cs
@@ -4,42 +4,40 @@ using System.Collections;
 public class Dash : MonoBehaviour
 {
 
-    private Rigidbody2D rigidbody2d;
+	private Rigidbody2D rigidbody2d;
 
-    public bool dash;
-    public float dashSpeed;
-    public float dashTime;
-    public float cooldown;
-    public int playerID;
+	public bool dash;
+	public float dashSpeed;
+	public float dashTime;
+	public float cooldown;
+	public int playerID;
 
-    private float dashEnd;
-    private float nextDash;
+	private float dashEnd;
+	private float nextDash;
 
-    void Start()
-    {
-        // Initialize variables.
-        rigidbody2d = GetComponent<Rigidbody2D>();
-    }
+	void Start ()
+	{
+		// Initialize variables.
+		rigidbody2d = GetComponent<Rigidbody2D> ();
+	}
 
-    void Update()
-    {
-        // Dash assignment and cooldown.
-        if (Input.GetButton("Dash" + playerID) && Time.time > nextDash)
-        {
-            dash = true;
-            dashEnd = Time.time + dashTime;
-            nextDash = Time.time + cooldown;
-        }
+	void Update ()
+	{
+		// Dash assignment and cooldown.
+		if (Input.GetButton (this.GetComponent<PlayerControls> ().dash) && Time.time > nextDash) {
+			dash = true;
+			dashEnd = Time.time + dashTime;
+			nextDash = Time.time + cooldown;
+		}
 
-        // Dashing state.
-        if (dash == true) {
-            rigidbody2d.AddForce(rigidbody2d.velocity * dashSpeed);
-        }
+		// Dashing state.
+		if (dash == true) {
+			rigidbody2d.AddForce (rigidbody2d.velocity * dashSpeed);
+		}
 
-        // Set dash to inactive after certain amount of time.
-        if (Time.time > dashEnd)
-        {
-            dash = false;
-        }        
-    }
+		// Set dash to inactive after certain amount of time.
+		if (Time.time > dashEnd) {
+			dash = false;
+		}        
+	}
 }
