f6c58111d2e6f2f4bfaaa8a7d4ae2fb4528c9599
Luca-Gazzola <lgazzola@uci.edu>
2017-02-24 18:40:38 -0800

    Basic UI Implementation

---
+++ Dazed & Confused/Assets/Scripts/GameUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+	private float time;
+	public float initialTime;
+
+	// Use this for initialization
+	void Start () {
+		this.time = initialTime;
+	}
+
+	void Update () {
+
+		// Timer
+		this.time -= Time.deltaTime;
+		GameObject.Find ("Timer").GetComponent<Text> ().text = "Time: " + string.Format ("{0:0}:{1:00}", Mathf.Floor (this.time / 60), Mathf.Floor (this.time) % 60);
+
+		// Player Score
+		GameObject.Find ("Player 1").GetComponent<Text> ().text = "Player 1: " + GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score.ToString();
+		GameObject.Find ("Player 2").GetComponent<Text> ().text = "Player 2: " + GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score.ToString();
+
+
+		if (this.time < 0) {
+			this.time = initialTime;
+			GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score = 0;
+			GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score = 0;
+		}
+	}
+}
