6067129386739e022e3cd2f52b86b39cea144185
Luca-Gazzola <lgazzola@uci.edu>
2017-03-04 17:48:30 -0800

    Updated branch

---
+++ Dazed & Confused/Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuNavigation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadScene(string destination)
+	{
+		Application.LoadLevel(destination);
+	}
+}
