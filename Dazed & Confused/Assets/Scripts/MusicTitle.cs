6ca00bd5017d357bd5602ee781d4d6dd25605295
Luca-Gazzola <lgazzola@uci.edu>
2017-03-04 17:25:05 -0800

    Title music and script

---
+++ Dazed & Confused/Assets/Scripts/MusicTitle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTitle : MonoBehaviour {
+
+	public AudioClip Music;
+	AudioSource audio; 
+
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		audio.Play (Music, 1f);
+	}
+}
