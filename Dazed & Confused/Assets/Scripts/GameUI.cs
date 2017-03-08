9aacb51413ff390c6d842f6b8d851e7c2f37e5fb
Bryce Tham <brycetham@gmail.com>
2017-03-08 14:31:20 -0800

    Completed Test Build

--- Dazed & Confused/Assets/Scripts/GameUI.cs
+++ Dazed & Confused/Assets/Scripts/GameUI.cs
@@ -27,15 +27,33 @@ public class GameUI : MonoBehaviour {
 		GameObject.Find ("Player 2").GetComponent<Text> ().text = "Player 2: " + GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score.ToString();
 
 
-		if (this.time < 0) {
+		if (this.time <= 1) {
+			this.time = 1;
 			this.GameOver ();
-//			this.time = initialTime;
-//			GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score = 0;
-//			GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score = 0;
+			//GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score = GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score.ToString();
+			//GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score = GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score.ToString();
 		}
 	}
 
 	private void GameOver() {
 		gameOverUI.SetActive (true);
+		//players are stunned when timer ends
+		GameObject.Find ("Player (1)").GetComponent<PlayerMovement> ().StartStun (999);
+		GameObject.Find ("Player (2)").GetComponent<PlayerMovement> ().StartStun (999);
+
+		//update player scores
+		GameObject.Find ("Player 1 Score").GetComponent<Text> ().text = GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score.ToString ();
+		GameObject.Find ("Player 2 Score").GetComponent<Text> ().text = GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score.ToString ();
+
+		//if player 2 is the winner
+		if (GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score < GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score) {
+			GameObject.Find ("Player 1 Results").GetComponent<Text> ().text = "LOSER";
+			GameObject.Find ("Player 2 Results").GetComponent<Text> ().text = "WINNER";
+
+		//if game is a draw
+		} else if (GameObject.Find ("Player (1)").GetComponent<PlayerData> ().score == GameObject.Find ("Player (2)").GetComponent<PlayerData> ().score) {
+			GameObject.Find ("Player 1 Results").GetComponent<Text> ().text = "DRAW";
+			GameObject.Find ("Player 2 Results").GetComponent<Text> ().text = "DRAW";
+		}
 	}
 }
